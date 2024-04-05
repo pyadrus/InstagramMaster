629b74d5270c2c7d4b795c7848c6942439319676
@PyAdminRU <zh.vitaliy92@gmail.com>
2024-04-06 00:07:26 +0300

    Структурирование кода

--- system/config.py
+++ system/config.py
@@ -21,16 +21,6 @@ def load_json_password(filename='system/instagram_password.json'):
     return password
 
 
-def load_json_proxy(filename='system/proxy.json'):
-    """
-    Чтение json файла
-    :args filename: - путь до файла
-    """
-    with open(filename, 'r') as f:
-        proxy = json.load(f)
-    return proxy
-
-
 def load_json_proxy_options(filename='system/proxy.json'):
     """
     Чтение json файла
@@ -39,16 +29,6 @@ def load_json_proxy_options(filename='system/proxy.json'):
     with open(filename, 'r') as f:
         proxy_options = json.load(f)
     return proxy_options
-
-
-def load_json_ClientID(filename='system/clientid.json'):
-    """
-    Чтение json файла
-    :args filename: - путь до файла
-    """
-    with open(filename, 'r') as f:
-        ClientID = json.load(f)
-    return ClientID
 
 
 def load_json_token(filename='system/token.json'):
@@ -69,23 +49,3 @@ def load_json_links(filename='system/links.json'):
     with open(filename, 'r') as f:
         links = json.load(f)
     return links
-
-
-def load_json_Client_secret(filename='system/Client_secret.json'):
-    """
-    Чтение json файла
-    :args filename: - путь до файла
-    """
-    with open(filename, 'r') as f:
-        Client_secret = json.load(f)
-    return Client_secret
-
-
-def load_json_passw(filename='system/passw.json'):
-    """
-    Чтение json файла
-    :args filename: - путь до файла
-    """
-    with open(filename, 'r') as f:
-        passw = json.load(f)
-    return passw
