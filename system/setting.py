d3ad45f0875e9371f25834fd1b75857dc2f01fa9
@PyAdminRU <zh.vitaliy92@gmail.com>
2024-04-14 17:21:26 +0300

    Добавлено запись ссылки на  Google Sheets в config.ini

--- system/setting.py
+++ system/setting.py
@@ -37,10 +37,23 @@ def update_config_disk(token) -> None:
         config.write(configfile)
 
 
+def update_config_disk_link(link) -> None:
+    """
+    Обновление link в файле config.ini.
+    :arg link: link instagram.
+    """
+    config = configparser.ConfigParser()
+    config.read('system/config.ini')
+    if not config.has_section('google_sheets_link'):
+        config.add_section('google_sheets_link')
+    config.set('google_sheets_link', 'link', link)
+
+
 def program_settings():
     """Меню настроек программы"""
     print('[1] Запись логина и пароля\n'
-          '[2] Запить токена Яндекс Диск\n')
+          '[2] Запись токена Яндекс Диск\n'
+          '[3] Запись ссылки на Google Sheets\n')
 
     user_input = input('Выберите вариант: ')
     if user_input == '1':
@@ -50,6 +63,9 @@ def program_settings():
     elif user_input == '2':
         token = input("Введите новый токен: ")
         update_config_disk(token)
+    elif user_input == '3':
+        link = input("Введите ссылку на Google Sheets: ")
+        update_config_disk_link(link)
     else:
         print('Вы ввели неверный номер')
         program_settings()
